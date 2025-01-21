3e6a162f0dd009b73aa6be3817cd150ca7cf4393
Kostiantyn Shestakov <kostiashestakov10@gmail.com>
2025-01-21 13:33:00 +0200

    imp

--- src/modules/AboutPage/AboutPage.tsx
+++ src/modules/AboutPage/AboutPage.tsx
@@ -1,11 +1,11 @@
 import { Form } from "../shared/Form";
 import styles from "./AboutPage.module.scss";
+import { AboutInfo } from "./components/AboutInfo";
 
 export const AboutPage = () => {
   return (
     <section className={`page__about-us  ${styles["about-us"]} padding-top`}>
-      <h2 className="about-us__title">About us</h2>
-
+      <AboutInfo />
       <Form />
     </section>
   );
